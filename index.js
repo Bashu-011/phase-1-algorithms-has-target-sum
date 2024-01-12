a04732c220d08040a0d34b885be2d198a904cdb3
Bashu-011 <bashu2609@gmail.com>
2024-01-12 08:57:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    // for the current number, identify a complementary number that adds to our target
+    // (for example: if our number is 2, and the target is 5, the complementary number is 3)
+    const complement = target - array[i];
+    // iterate over the remaining numbers in the array
+    for (let j = i + 1; j < array.length; j++) {
+      // check if any of the remaining numbers is the complement
+      // if so, return true
+      if (array[j] === complement) 
+      return true;
+    }
+  }
+  // if we reach the end of the array, return false
+  return false;
 }
+
 
 /* 
   Write the Big O time complexity of your function here
+  5(5*n)
+  If you remove the constatnts 0(n)
+
 */
 
 /* 
   Add your pseudocode here
+  *START
+  * A function hasTargetSum that takes two inputs
+  * The functionthen loops through the array
+  * Create a variable and store the difference
+  * Check if the remaining value is equal to the remainder
 */
 
 /*
   Add written explanation of your solution here
+  Take the array
+  Using a for loop and loop through it
+  Get the difference between the taarget number and values in the array
+  Get the remainder and compare it to the target
 */
 
 // You can run `node index.js` to view these console logs
